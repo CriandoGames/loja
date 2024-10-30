9aea45c5562448cd893f87f9b7e2824047af8255
Carlos Castro <carloscastrogames@gmail.com>
2024-10-30 19:35:36 -0300

    layout

--- lib/features/home_store/home_store.dart
+++ lib/features/home_store/home_store.dart
@@ -23,15 +23,42 @@ class _HomeStoreState extends State<HomeStore> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Products'),
+        backgroundColor: Colors.white,
         actions: [
           IconButton(
             icon: const Icon(Icons.favorite_outline),
             onPressed: () {},
-          )
+          ),
         ],
+        bottom: PreferredSize(
+          preferredSize:
+              const Size.fromHeight(80), // Altura da barra de pesquisa
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 30, right: 30, bottom: 16, top: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search Anything',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.grey[100],
+              ),
+              onChanged: (value) {
+                // Implementação para pesquisa
+                ;
+              },
+            ),
+          ),
+        ),
       ),
       body: ValueListenableBuilder<HomeStoreState>(
         valueListenable: controller,
@@ -45,7 +72,59 @@ class _HomeStoreState extends State<HomeStore> {
               itemCount: state.products.length,
               itemBuilder: (_, index) {
                 final product = state.products[index];
-                return Text(product.name);
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    width: size.width * 0.1,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.network(
+                              product.imageUrl,
+                              width: 126,
+                              height: 121,
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(product.name),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          const Icon(Icons.star_border),
+                                          Text(product.rating.rate.toString()),
+                                          Text(
+                                              "(${"${product.rating.reviewCount} reviews"})"),
+                                        ],
+                                      ),
+                                      IconButton(
+                                          onPressed: () {},
+                                          icon: const Icon(
+                                              Icons.favorite_outline))
+                                    ],
+                                  ),
+                                  Text("\$${product.price.toString()}"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        const Divider(
+                          height: 1,
+                          thickness: 1,
+                        )
+                      ],
+                    ),
+                  ),
+                );
               },
             );
           } else if (state is HomeStoreStateEmpty) {
